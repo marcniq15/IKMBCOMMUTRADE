d58516150e348b90fda50827a5bcef48d8155dcf
marcniq15 <aniqpt01@gmail.com>
2025-09-01 22:14:23 +0800

    Update config.js

--- build/web/assets/assets/config.js
+++ build/web/assets/assets/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', //
-        region: ''
+        userPoolId: 'us-east-1_DKGPQeiej', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '26gs8tfvu3c1v8711d4mqkgkas', //
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: ''
         }
+
     };
